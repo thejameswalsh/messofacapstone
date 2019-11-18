6c7ecdb79b964ea69c292cb6a5b8cb0c9b59c366
thejameswalsh <35312608+thejameswalsh@users.noreply.github.com>
2019-11-19 00:01:49 +1100

    adding data generated from matlab

--- matlab feature adding/WERfromtable.m
+++ matlab feature adding/WERfromtable.m
@@ -9,9 +9,6 @@ last_number = 500
 
 %WER only works with char not string as striing makes it " needed '
 for i=1:last_number
-   i;
-    A = char(T(i+1, 2))
-    B = char(T(i + 1, 3))
    w = WER(char(T(i+1, 2)), char(T(i+1, 3)))
    num = count(T(i+1, 2),' ') + 1;
     t(i+1,1) = i;
